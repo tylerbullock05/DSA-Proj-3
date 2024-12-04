be5618cb85d553705f8d83b4cede6f598dba5647
joshuahpribell <167570611+joshuahpribell@users.noreply.github.com>
2024-12-03 21:31:07 -0500

    Making algorithms functional

--- main.cpp
+++ main.cpp
@@ -32,19 +32,33 @@ int main()
     // Run the two algorithms and start the clocks
     auto start = chrono::high_resolution_clock::now();
     // Create instance of dijkstra and perform the algorithm
+    int dijkstraDistance = graph.dijkstra(origin, destination);
 
     auto end = chrono::high_resolution_clock::now();
     auto dijkstraTime = chrono::duration_cast<chrono::milliseconds>(end - start);
+    // If a path exists, print the shortest distance
+    if (dijkstraDistance != -1) {
+        cout << "Dijkstra's Algorithm: Shortest distance from " << origin << " to " << destination << " is " << dijkstraDistance << " miles." << endl;
+    }
+    else {
+        cout << "Dijkstra's Algorithm: No path exists from " << origin << " to " << destination << "." << endl;
+    }
     cout << "Dijkstra time: " << dijkstraTime.count() << "ms" << endl;
 
     // Run the Bellman-Ford
     auto start2 = chrono::high_resolution_clock::now();
-    // Create instance of dijkstra and perform the algorithm
-
+    // Create instance of bellman ford and perform the algorithm
+    int bellmanFordDistance = graph.bellmanFord(origin, destination);
+    
     auto end2 = chrono::high_resolution_clock::now();
     auto bellmanFordTime = chrono::duration_cast<chrono::milliseconds>(end2 - start2);
+    // If a path exists, print the shortest distance
+    if (bellmanFordDistance != -1) {
+        cout << "Bellman-Ford Algorithm: Shortest distance from " << origin << " to " << destination << " is " << bellmanFordDistance << " miles." << endl;
+    } else {
+        cout << "Bellman-Ford Algorithm: No path exists from " << origin << " to " << destination << "." << endl;
+    }
     cout << "Bellman-Ford time: " << bellmanFordTime.count() << "ms" << endl;
-
 
     return 0;
 }
