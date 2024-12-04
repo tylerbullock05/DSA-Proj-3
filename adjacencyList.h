10f37b512fad95c4e50fbe2ab401dca829d8733a
Tyler Bullock <tylerbullock@ufl.edu>
2024-12-03 19:07:28 -0500

    Added parsing function for csv file and added in edges

--- adjacencyList.h
+++ adjacencyList.h
@@ -24,4 +24,4 @@ class AdjacencyList {
                 graph[to] = {};
             }
         }
-}
+};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "parseCSV.h"
+#include "adjacencyList.h"
 #include <vector>
 #include <iostream>
 using namespace std;
@@ -6,11 +7,13 @@ using namespace std;
 int main()
 {
     parseCSV file;
-    vector<vector<string>> f = file.parseData("USAAirlinesRoutes.csv");
-    for(auto flights : f)
+    AdjacencyList graph;
+    vector<vector<string>> flights = file.parseData("USAAirlinesRoutes.csv");
+    for(auto flight : flights)
     {
-        cout<<flights[0]<<" to "<<flights[1]<<" has a distance of "<<flights[2]<<" miles"<<endl;
+        graph.addEdge(flight[0],flight[1],stoi(flight[2]));
     }
+    cout<<"done"<<endl;
 
     return 0;
 }
